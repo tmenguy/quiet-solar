9f28df57977225a69492813a647821de676fcd77
tmenguy <thomas.menguy@gmail.com>
2024-07-06 03:04:18 +0200

    config flow begining... save

--- custom_components/quiet_solar/sensor.py
+++ custom_components/quiet_solar/sensor.py
@@ -1,7 +1,11 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-
+from .const import (
+    DATA_DEVICE_IDS,
+    DOMAIN,
+    DATA_HANDLER, DEVICE_TYPE
+)
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -9,5 +13,10 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the Netatmo switch platform."""
+    device = hass.data[DOMAIN][entry.entry_id]
+    data_handler = hass.data[DOMAIN][DATA_HANDLER]
+
+    #home is not necessarily set already ... but depending on the device we will creat e various sensors....
+
 
     return
