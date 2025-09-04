9a40860794c6e25d2eaf1ec34aab03feb8d88167
tmenguy <thomas.menguy@gmail.com>
2025-09-04 22:38:49 +0200

    dashboard std

--- custom_components/quiet_solar/ui/dashboard.py
+++ custom_components/quiet_solar/ui/dashboard.py
@@ -15,6 +15,7 @@ _LOGGER = logging.getLogger(__name__)
 async def generate_dashboard_yaml(home):
     base_dir = os.path.join(os.path.dirname(__file__))
     jinja_path = os.path.join(base_dir, "quiet_solar_dashboard_template.yaml.j2")
+    jinja_path_standard = os.path.join(base_dir, "quiet_solar_dashboard_template_standard_ha.yaml.j2")
 
     _LOGGER.warning("generate_dashboard_yaml: regenerating dashboard YAML file")
 
@@ -22,25 +23,35 @@ async def generate_dashboard_yaml(home):
     async with aiofiles.open(jinja_path, "r") as f:
         template_content = await f.read()
 
+    async with aiofiles.open(jinja_path_standard, "r") as f:
+        template_content_standard = await f.read()
+
     tpl = Template(template_content, home.hass)
+    tpl_std = Template(template_content_standard, home.hass)
 
     try:
         rendered = tpl.async_render(variables={"home": home})
+        rendered_std = tpl_std.async_render(variables={"home": home})
 
         storage_path: str = os.path.join(home.hass.config.path(), DOMAIN)
         await aiofiles.os.makedirs(storage_path, exist_ok=True)
-        base_dir = os.path.join(storage_path, "quiet_solar_dashboard.yaml")
-        async with aiofiles.open(base_dir, "w") as f_out:
+
+        dash_path = os.path.join(storage_path, "quiet_solar_dashboard.yaml")
+        async with aiofiles.open(dash_path, "w") as f_out:
             await f_out.write(rendered)
 
-        storage_path_resources_destintion: str = os.path.join(home.hass.config.path(), "www", DOMAIN)
-        await aiofiles.os.makedirs(storage_path_resources_destintion, exist_ok=True)
+        dash_path = os.path.join(storage_path, "quiet_solar_dashboard_standard.yaml")
+        async with aiofiles.open(dash_path, "w") as f_out:
+            await f_out.write(rendered_std)
+
+        storage_path_resources_destination: str = os.path.join(home.hass.config.path(), "www", DOMAIN)
+        await aiofiles.os.makedirs(storage_path_resources_destination, exist_ok=True)
 
         resources_from_path: str = os.path.join(os.path.dirname(__file__), "resources")
 
         for entry in await aiofiles.os.listdir(resources_from_path):
             source_path = os.path.join(resources_from_path, entry)
-            destination_path = os.path.join(storage_path_resources_destintion, entry)
+            destination_path = os.path.join(storage_path_resources_destination, entry)
 
             if await aiofiles.os.path.isfile(source_path):
                 async with aiofiles.open(source_path, "rb") as src, aiofiles.open(destination_path, "wb") as dest:
